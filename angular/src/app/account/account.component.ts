e97cd98e7a597f1a52402167ae00f21fdab57583
iGor Polyakov <igor_polyakov@phystech.edu>
2018-08-01 20:19:59 +0300

    account updated

--- angular/src/app/account/account.component.ts
+++ angular/src/app/account/account.component.ts
@@ -7,6 +7,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent implements OnInit {
+  tabs = [
+    {
+      title: "contacts",
+      titleRus: "Контакты"
+    },
+    {
+      title: "schedule",
+      titleRus: "Расписание"
+    },
+    {
+      title: "marks",
+      titleRus: "Успеваемость"
+    },
+    {
+      title: "sections",
+      titleRus: "Секции"
+    }
+  ];
+  selectedTab = this.tabs[0].title;
+
+  kids = [
+    {
+      name: "Вася",
+      package: "Full"
+    },
+    {
+      name: "Оля",
+      package: "Бич"
+    }
+  ];
+
   constructor(
     private router: Router
   ) {}
@@ -16,5 +47,10 @@ export class AccountComponent implements OnInit {
 
   logout() {
     this.router.navigate(['/']);
+  }
+
+  selectTab(event, tab) {
+    event.preventDefault();
+    this.selectedTab = tab;
   }
 }
